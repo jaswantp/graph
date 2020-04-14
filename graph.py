91bbb7ed2b737279740753acc0985af492df6a9b
jaswantp <40538987+jaswantp@users.noreply.github.com>
2020-04-14 12:06:58 +0200

    new feature methods.
    1. a staticmethod pairwise.
    2. a method to move nodes. ignores if node is not a tuple(x,y).
    3. connectedComps is a property. It can be evaluated when required.
    4. vertIndices is a property. It's evaluated when required.
    5. edgeIndices is a property. It's also evaluated when required.

--- graph.py
+++ graph.py
@@ -1,12 +1,12 @@
 """@package Graph
-A convenient class to handle a graph with nodes and edges.
+A convenient class to handle an undirected graph with nodes connected by edges.
 """
-
+import itertools
 import logging
-from collections import defaultdict
-from itertools import combinations
-
+import operator
 import typing
+from collections import defaultdict
+from itertools import combinations, chain
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO, format='%(message)s')
@@ -26,8 +26,18 @@ class Graph:
     def __init__(self):
         self.graph = defaultdict(set)  # {node: set(adjacency list)}
         self.newnodes = set()
-        self.connectedComps = []
-        self.indices = {}
+        self.__connectedComps = []
+        self.__vertIndices = {}
+        self.__edgeIndices = set()
+
+    @staticmethod
+    def pairwise(iterable):
+        """
+        s -> (s0,s1), (s1,s2), (s2, s3), ... [https://docs.python.org/3/library/itertools.html#itertools-recipes]
+        """
+        a, b = itertools.tee(iterable)
+        next(b, None)
+        return zip(a, b)
 
     def nodeExists(self, node: typing.Any):
         return True if self.graph[node] else False
@@ -42,6 +52,12 @@ class Graph:
     def deleteEdge(self, node1: typing.Any, node2: typing.Any):
         self.graph[node1].discard(node2)
         self.graph[node2].discard(node1)
+
+    def deleteNode(self, node: typing.Any):
+        self.graph[node].clear()  # Clear the node's adjacency list.
+        del self.graph[node]  # Delete the dict key.
+        for _node in self.graph:
+            self.graph[_node].discard(node)  # discard node from other node's adjacency list.
 
     def dissolveNode(self, node: typing.Any):
         self.graph[node].clear()  # Clear the node's adjacency list.
@@ -104,26 +120,28 @@ class Graph:
 
         return component
 
-    def findConnectedComps(self):
+    @property
+    def connectedComps(self):
         """
         Call this method when an edge is added or at once after all edges are added.
         Python is beautiful and all but unlike c++ it doesn't do tail call optimizations.
         So, this method automatically switches to iterative algorithm on a recursion error.
         Returns: None
         """
-        self.connectedComps.clear()
+        self.__connectedComps.clear()
         try:
             visited = []
             for node in self.graph:
                 if node not in visited:
                     component = []
-                    self.connectedComps.append(self.dfsrecrusive(node, component, visited))
+                    self.__connectedComps.append(self.dfsrecrusive(node, component, visited))
         except RecursionError:
             logger.info("Switched to iterative")
             visited = []
             for node in self.graph:
                 if node not in visited:
-                    self.connectedComps.append(self.dfsiterative(node, visited))
+                    self.__connectedComps.append(self.dfsiterative(node, visited))
+        return self.__connectedComps
 
     def areConnected(self, node1: typing.Any, node2: typing.Any):
         """
@@ -142,25 +160,70 @@ class Graph:
                 continue
         return False
 
-    def generateIndices(self):
+    def moveNode(self, targNode: tuple, offset: tuple):
+        """
+        Moves targNode by offset. The types of targNode and offset must conform to a tuple.
+        Note:
+            If targNode is a tuple(x, y), it is understood that offset is a tuple(dx, dy)
+            This method makes sense only if the nodes are x,y positions.
+        Args:
+            targNode (tuple): target node that is to be moved by an offset
+            offset (tuple): the offset
+
+        Returns: None
+
+        """
+        try:
+            assert isinstance(targNode, tuple)
+            assert isinstance(offset, tuple)
+        except AssertionError:
+            return 
+        adjNodes = self.graph.pop(targNode)  # helps in reconnection with adjacent nodes after target node is moved.
+        self.deleteNode(targNode)  # delete target node.
+        targNode = tuple(map(operator.add, targNode, offset))  # add offset to target node.
+        for node in adjNodes:
+            # Reconnect targNode to nodes in it's adjacency list.
+            self.addEdge(targNode, node)
+
+    @property
+    def vertIndices(self):
         """
         Generates table of indices for nodes. key : value ==> node : index
         Can fetch index of a node. with self.indices.get(node)
         """
+        self.__vertIndices.clear()
         for i, node in enumerate(self.graph):
-            self.indices.update({node: i})
+            self.__vertIndices.update({node: i})
+        return self.__vertIndices
+
+    @property
+    def edgeIndices(self):
+        """
+        Iterate over adjacency list to find number of unique edges.
+        Returns: int
+
+        """
+        self.__edgeIndices = set()
+        for node in self.graph.keys():
+            for adjnode in self.graph[node]:
+                self.__edgeIndices.add((self.vertIndices.get(node), self.vertIndices.get(adjnode)))
+
+        self.__edgeIndices.intersection_update(combinations(list(self.vertIndices.values()), 2))
+        self.__edgeIndices = list(chain.from_iterable(self.__edgeIndices))  # convert set to a continuous list.
+        return self.__edgeIndices
 
 
 if __name__ == '__main__':
     import timeit
 
     dfsAlgo = """
-from graph import Graph
+from editor.graph import Graph
 g = Graph()
 for i in range(1_000_000):
     g.addEdge((i, i), (i + 1, i + 1))
-g.findConnectedComps()
-print("No. of connected components: {0}".format(len(g.connectedComps)))
+g.find__connectedComps()
+print("No. of connected components: {0}".format(len(g.__connectedComps)))
 """
     time = timeit.timeit(dfsAlgo, number=1)
-    print("Executed in {0}".format(time))
+    logger.info("Executed in {0}".format(time))
+
